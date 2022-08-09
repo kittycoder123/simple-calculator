22e69a3c38afbc15d1abf8aef181aec17331fc79
kittycoder123 <110904871+kittycoder123@users.noreply.github.com>
2022-08-09 16:55:06 +0530

    Add files via upload

---
+++ simple calculator.py
@@ -0,0 +1,13 @@
+print("simple calculator")
+x=int(input("enter the first number"))
+y=int(input("enter the second number"))
+sum=x+y
+difference=x-y
+quotient=x/y
+product=x*y
+remainder=x%y
+print("the sum of x and y is",sum)
+print("difference of x and y is",difference)
+print("the quotient of x and y is",quotient)
+print("the product of x and y is",product)
+print("the remainder of x and y is",remainder)
